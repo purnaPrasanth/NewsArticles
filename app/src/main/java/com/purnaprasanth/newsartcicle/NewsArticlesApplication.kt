4a49bb1de41919bd896222438623e5552aae8e9b
purna <purna.prasanth@xische.com>
2019-11-03 13:24:57 +0400

    Initial Setup Bug Fixed

--- app/src/main/java/com/purnaprasanth/newsartcicle/NewsArticlesApplication.kt
+++ app/src/main/java/com/purnaprasanth/newsartcicle/NewsArticlesApplication.kt
@@ -1,9 +1,8 @@
 package com.purnaprasanth.newsartcicle
 
-import com.purnaprasanth.baseandroid.appinitializers.AppInitializers
+import com.purnaprasanth.newsartcicle.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
-import javax.inject.Inject
 
 /**
  * Created by viking_93 on 2019-11-03
@@ -11,16 +10,8 @@ import javax.inject.Inject
 
 class NewsArticlesApplication : DaggerApplication() {
 
-    @Inject
-    lateinit var initializers: AppInitializers
-
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.factory().create(this)
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        initializers.init(this)
     }
 
 }
