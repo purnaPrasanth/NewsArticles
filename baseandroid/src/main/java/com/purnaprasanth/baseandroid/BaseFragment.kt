10fa11261341e31dceefddab6fd35dfe08e9dd77
purna <purna.prasanth@xische.com>
2019-11-04 20:17:32 +0400

    Refactoring Build Files

--- baseandroid/src/main/java/com/purnaprasanth/baseandroid/BaseFragment.kt
+++ baseandroid/src/main/java/com/purnaprasanth/baseandroid/BaseFragment.kt
@@ -8,13 +8,12 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModelProvider
+import com.purnaprasanth.base.AppDispatchers
 import dagger.android.support.DaggerFragment
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
-
 
 /**
  * Created by viking_93 on 2019-11-03
@@ -22,30 +21,35 @@ import kotlin.coroutines.CoroutineContext
 
 abstract class BaseFragment<BINDING : ViewDataBinding>(@LayoutRes val layId: Int) :
     DaggerFragment(), CoroutineScope {
-    private lateinit var bindingInternal: BINDING
-    val binding: BINDING
-        get() = bindingInternal
+
+    private lateinit var _binding: BINDING
+
+    protected val binding: BINDING
+        get() = _binding
 
     private val job = SupervisorJob()
     override val coroutineContext: CoroutineContext
-        get() = job + Dispatchers.Main
+        get() = job + appDispatchers.mainDispatcher
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    @Inject
+    lateinit var appDispatchers: AppDispatchers
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        bindingInternal = DataBindingUtil.inflate(inflater, layId, container, false)
-        bindingInternal.lifecycleOwner = this
+        _binding = DataBindingUtil.inflate(inflater, layId, container, false)
+        _binding.lifecycleOwner = this
         initUI()
         return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
+    override fun onDetach() {
+        super.onDetach()
         job.cancel()
     }
 
